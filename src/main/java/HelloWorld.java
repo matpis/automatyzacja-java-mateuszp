336c81cc8891adc289190d4c5bf2a523a192bc02
Mateusz Piskorski <33571747+matpis@users.noreply.github.com>
2017-11-11 17:58:12 +0100

    Add files via upload

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("*Jeżeli chcesz wyjść z programu wpisz \"Exit\" " +
+                "i następnie wciśnij klawisz Enter.");
+        System.exit(0);
+    }
+}
