2f64c5d165e0107b4864269a22312035859be0d0
Mateusz Piskorski <33571747+matpis@users.noreply.github.com>
2017-11-12 21:19:58 +0100

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("*Jeżeli chcesz wyjść z programu wpisz \"Exit\" " +
-                "i następnie wciśnij klawisz Enter.");
-        System.exit(0);
+        Scanner sc = new Scanner(System.in);
+        String user_command;
+        System.out.println("Witaj świecie!");
+        do {
+            System.out.println("*Jeżeli chcesz wyjść z programu wpisz \"Exit\", a następnie wciśnij klawisz Enter.");
+            user_command = sc.nextLine();
+        } while (!user_command.equals("Exit"));
     }
 }
